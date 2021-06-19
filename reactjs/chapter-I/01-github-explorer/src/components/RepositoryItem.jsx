3c75420c620b569881cf0bcdb0c76b92eeec539c
Gabriel Coelho <Coelho.Gabriel@bcg.com>
2021-06-19 02:57:17 -0300

    feat(reactjs): add GitHub API to fetch the repos

--- reactjs/chapter-I/01-github-explorer/src/components/RepositoryItem.jsx
+++ reactjs/chapter-I/01-github-explorer/src/components/RepositoryItem.jsx
@@ -4,7 +4,7 @@ export function RepositoryItem(props) {
       <strong>{props.repository.name}</strong>
       <p>{props.repository.description}</p>
 
-      <a href={props.repository.link}>Access</a>
+      <a href={props.repository.html_url}>Access</a>
     </li>
   );
 }
--- reactjs/chapter-I/01-github-explorer/src/components/RepositoryList.jsx
+++ reactjs/chapter-I/01-github-explorer/src/components/RepositoryList.jsx
@@ -1,26 +1,26 @@
-import { RepositoryItem } from "./RepositoryItem"
+import { RepositoryItem } from "./RepositoryItem";
 
-const dbtRepository = {
-  name: 'dbt',
-  description: 'A transformation tool.', 
-  link: 'https://github.com/'
-}
-
-const jenkinsRepository = {
-  name: 'Jenkins',
-  description: 'Implement a CI/CD with Jenkins.', 
-  link: 'https://github.com/'
-}
+import "../styles/repositories.scss";
+import { useEffect, useState } from "react";
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Repositories List</h1>
 
       <ul>
-        <RepositoryItem repository={dbtRepository} />
-        <RepositoryItem repository={jenkinsRepository}/>
+        {repositories.map((repository) => (
+          <RepositoryItem key={repository.id} repository={repository} />
+        ))}
       </ul>
     </section>
-  )
+  );
 }
